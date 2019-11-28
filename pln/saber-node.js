6a983220e4c768094035331cb15a306508462abd
日天 <gaoryrt@users.noreply.github.com>
2019-11-28 15:16:40 +0800

    ⚡️ lighthouse improve

--- pln/saber-node.js
+++ pln/saber-node.js
@@ -7,5 +7,8 @@ exports.chainWebpack = function(chain) {
 
 exports.getDocument = html => html.replace(
   'width=device-width, initial-scale=1.0',
-  'width=device-width,initial-scale=1,maximum-scale=1,minimum-scale=1,user-scalable=no,viewport-fit=cover'
+  'width=device-width,initial-scale=1,minimum-scale=1,user-scalable=no,viewport-fit=cover'
+).replace(
+  '<html data-saber-ssr>',
+  '<html lang=zh-Hans data-saber-ssr>'
 )
