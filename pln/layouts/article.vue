dab0b92596285953c404f23d410f1cdd4894ae1f
日天 <gaoryrt@users.noreply.github.com>
2019-12-14 17:18:21 +0800

    :bug: comment

--- pln/layouts/article.vue
+++ pln/layouts/article.vue
@@ -21,9 +21,28 @@
           <slot name="default"/>
         </div>
       </div>
-      <script id="dsq-count-scr" src="//thejungle-1.disqus.com/count.js" async></script>
+      <div id="disqus_thread"></div>
     </div>
     <Footer/>
+    <script>
+
+/**
+*  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+*  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+/*
+var disqus_config = function () {
+this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+};
+*/
+(function() { // DON'T EDIT BELOW THIS LINE
+var d = document, s = d.createElement('script');
+s.src = 'https://thejungle-1.disqus.com/embed.js';
+s.setAttribute('data-timestamp', +new Date());
+(d.head || d.body).appendChild(s);
+})();
+</script>
+<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
   </div>
 </template>
 
