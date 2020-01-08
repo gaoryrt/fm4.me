658cdc717f5eacc9352b0c0c5b64443a36a4a210
gaoryrt <gaoryrt@gmail.com>
2020-01-08 18:24:55 +0800

    :bug: sizin

--- pln/components/audioTitle.vue
+++ pln/components/audioTitle.vue
@@ -282,10 +282,10 @@ export default {
 }
 .info {
   font-family: thinpixel;
-  font-size: 24px;
-  line-height: 24px;
+  font-size: 28px;
+  line-height: 1;
   text-align: left;
-  width: 115px;
+  width: 100%;
   white-space: nowrap;
   text-overflow: ellipsis;
   overflow: hidden;
@@ -398,6 +398,12 @@ export default {
   }
   .left, .right {
     width: 100%;
+  }
+  .glass {
+    padding: 20px 5vw;
+  }
+  .info {
+    font-size: 6vw;
   }
 }
 </style>
