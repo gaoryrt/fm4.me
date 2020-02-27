24de88eda6371c0675174fb43eb25931c1539818
gaoryrt <gaoryrt@gmail.com>
2020-02-27 12:12:36 +0800

    📈 analyze

--- pln/saber-node.js
+++ pln/saber-node.js
@@ -5,3 +5,11 @@ exports.getDocument = html =>
       'width=device-width,initial-scale=1,minimum-scale=1,user-scalable=no,viewport-fit=cover'
     )
     .replace('<html data-saber-ssr>', '<html lang=zh-Hans data-saber-ssr>')
+    .replace('</head>', `<!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-159221418-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'UA-159221418-1');
+    </script>`)
