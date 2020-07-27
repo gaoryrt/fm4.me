b45bd1409f6a6638223041b1c7d4b92aac426693
gaoryrt <gaoryrt@gmail.com>
2020-07-27 16:25:22 +0800

    ✨ new doc

--- saber-config.js
+++ saber-config.js
@@ -7,13 +7,9 @@ module.exports = {
   siteConfig: {
     url: 'https://jungle.fm',
     title: 'theJungle',
-    description: `The Jungle 由两个（终将不会年轻的）年轻人——王丹妮和高日天于 2017 年 8 月创办，
-是一档（暂时）没有主题的播客节目，两个人用声音记录他们（非极端）的思想和生活。
+    description: `The Jungle 由两个（终将不会年轻的）年轻人——王丹妮和高日天于 2017 年 8 月创办，是一档（暂时）没有主题的播客节目，两个人用声音记录他们（非极端）的思想和生活。
 
-The Jungle 的名字来自于「concrete jungle」，有「年轻人在钢筋丛林中求生」之意。
-当然 The Jungle 也是旺盛的热带雨林，我们希望永远对灿烂广大的生活保持生命力，好奇心。
-巧合的是，这与枪花乐队的《welcome to the jungle》一曲表达了类似的思想。
-Welcome to the jungle 正是在欢迎你收听本档节目。也是在欢迎你跟我们一起来到这个人间丛林。
+The Jungle 的名字来自于「concrete jungle」，有「年轻人在钢筋丛林中求生」之意。当然 The Jungle 也是旺盛的热带雨林，我们希望永远对灿烂广大的生活保持生命力，好奇心。巧合的是，这与枪花乐队的《welcome to the jungle》一曲表达了类似的思想。Welcome to the jungle 正是在欢迎你收听本档节目。也是在欢迎你跟我们一起来到这个人间丛林。
 
 The Jungle 主要使用音频播客为载体，不定期更新图片与文字版本；主要是两个主播在录，不定期邀请朋友同录。
 
